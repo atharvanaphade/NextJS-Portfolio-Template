c23f94ad84d9dee1654ed042e225612fbae28c1c
Atharva Naphade <naphade21@gmail.com>
2021-12-18 10:36:23 +0530

    Added theme button

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,28 @@
-import { Text, VStack } from "@chakra-ui/layout";
+import { Text, VStack, HStack } from "@chakra-ui/layout";
+import { FaSun, FaCloudMoon } from 'react-icons/fa';
+import { IconButton } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/color-mode";
+
+const ThemeButton = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return (
+        <IconButton 
+            onClick={toggleColorMode}
+            icon={colorMode === "light" ? <FaSun /> : <FaCloudMoon />}
+        />
+    )
+}
 
 const Footer = () => {
     return (
         <VStack alignItems='center' bg="blackAlpha.900" py={['5px', '5px', '20px']}>
-            <Text alignItems='center' color='gray.300' fontSize={['xl', 'lg', '2xl']}>
-                Copyright © 2021 | Atharva Naphade
-            </Text>
+            <HStack>
+                <ThemeButton />
+                <Text alignItems='center' color='gray.300' fontSize={['xl', 'lg', '2xl']}>
+                    Copyright © 2021 | Atharva Naphade
+                </Text>
+            </HStack>
         </VStack>
     );
 }
