c1c08c1646b50bc8079fad85f897432152016807
Atharva Naphade <naphade21@gmail.com>
2022-10-13 09:18:39 +0530

    Remove unused variables

--- pages/_app.js
+++ pages/_app.js
@@ -9,18 +9,6 @@ import { DefaultSeo } from 'next-seo';
 import SEO from '../components/seo-config';
 import { AnimatePresence } from 'framer-motion';
 
-const colors = {
-    brand: {
-        900: '#2A363B',
-        800: '#E84A5F',
-        700: '#FF847C',
-        600: '#FECEA8',
-        500: '#99B898',
-    },
-}
-
-const theme = extendTheme({ colors });
-
 function MyApp({ Component, pageProps, router }) {
 
   return (
