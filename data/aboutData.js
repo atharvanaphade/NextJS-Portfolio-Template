5534ef5d6d29625ff4d8a049bcd1c139e253115a
Atharva Naphade <naphade21@gmail.com>
2024-02-05 01:41:48 +0530

    Update aboutData.js

--- data/aboutData.js
+++ data/aboutData.js
@@ -2,7 +2,7 @@ const about = {
     name: 'Atharva Naphade',
     about: 'Hi👋, I am a Software Engineer working at Samsung Semiconductor India - Bangalore, building firmware for flagship Samsung phones.I have completed my undergraduate degree in Electronics and Telecommunications Engineering from Pune Institute of Computer Technology, with a keen interest in Software Development and Mathematics. I love learning new technologies and applying them in different scenarios, where they might help in solving real world problems.',
     headline: 'Software Engineer',
-    typewriter: ['Full-Stack Developer', 'Mobile App Developer', 'DevOps Engineer'],
+    typewriter: ['Full-Stack Developer', 'Mobile App Developer', 'Firmware Engineer'],
     links: [
         { name: 'GitHub', link: 'https://github.com/atharvanaphade' },
         { name: 'Instagram', link: 'https://www.instagram.com/atharvanaphade/' },
